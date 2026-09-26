bd23a834447bc4277f81ac808f227051289e745c
manandrazanaeraelien-dev <manandrazanaeraelien@gmail.com>
2026-09-26 04:14:05 +0300

    Fix base URL for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  base: '/l2-gl-2026-groupe-01-todo-app/'
 })
